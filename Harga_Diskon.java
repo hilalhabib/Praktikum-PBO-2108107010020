9f81bfb02e176125cb41f28426f4ee055724ecee
hilalhabib <102476649+hilalhabib@users.noreply.github.com>
2022-09-06 19:45:02 +0700

    Add files via upload

---
+++ Harga_Diskon.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Harga_Diskon {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int harga, jumlah, jmlhrg, diskon, total;
+
+        System.out.print("Harga barang = ");
+        harga = input.nextInt();
+        System.out.print("Jumlah barang = ");
+        jumlah = input.nextInt();
+
+        jmlhrg = harga*jumlah;
+        diskon = jmlhrg*15/100;
+        total  = jmlhrg-diskon;
+
+        System.out.println("Total harga disc 15% adalah " +harga+" * " +jumlah+ " = " +total);
+
+    }
+} 
